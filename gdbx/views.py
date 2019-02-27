887ae15d824c990067d632c948890c538fba659f
vineeth0791 <vineethkumar0791@gmail.com>
2019-02-27 11:23:19 +0530

    create campaign

--- gdbx/views.py
+++ gdbx/views.py
@@ -6,7 +6,8 @@ def getGDbxxx(request):
         #full_path = ('http', ('', 's')[request.is_secure], '://', request.META[0], );
         #full_path = ''.join(full_path);
         host = request.META['HTTP_HOST'];
+        REMOTE_USER = request.META['REMOTE_USER'];
         #return JsonResponse({'status':'true','full_path':full_path});
-        return JsonResponse({'full_path':(host)});
+        return JsonResponse({'full_path':(host),'REMOTE_HOST':request.META['REMOTE_HOST'],'REMOTE_USER':request.META['REMOTE_USER'],"SERVER_NAME":request.META['SERVER_NAME']});
     else:
         return JsonResponse({'status':'false'});
