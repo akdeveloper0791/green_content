ffb457eac1bd894d9f9bcdf65b78bc55d8c2a59a
vineeth0791 <vineethkumar0791@gmail.com>
2019-03-01 19:38:18 +0530

    group assign notifications always on task

--- group/SendGroupAssignNotificationsBG.py
+++ group/SendGroupAssignNotificationsBG.py
@@ -1,4 +1,10 @@
 from time import sleep
+from models import GroupMemberAssignNotification
+
+#list latest 100 pending email requests
+pendingList = GroupMemberAssignNotification.objects.all().order_by(id)[:100];
+for notification in pendingList.iterator():
+    print(notification.members)
 
 print("INside Send Group Assign notifications",flush=True);
 sleep(300);#sleep for 5 mintes
