625d200df8c134cd5549e03fd59d221900669e58
vineeth0791 <vineethkumar0791@gmail.com>
2019-03-01 20:52:45 +0530

    always on tasks

--- group/SendGroupAssignNotificationsBG.py
+++ group/SendGroupAssignNotificationsBG.py
@@ -1,18 +1,10 @@
 from time import sleep
+from models import GroupMemberAssignNotification
 
-activate_this = "home/adskite/.virtualenvs/cms/bin/activate_this.py"
-execfile(activate_this, dict(__file__=activate_this))
-
-
-from cmsapp.models import User_unique_id,Multiple_campaign_upload
-
-#from models import GroupMemberAssignNotification
-#from sys.path import User_unique_id
 #list latest 100 pending email requests
-'''pendingList = GroupMemberAssignNotification.objects.all().order_by(id)[:100];
+pendingList = GroupMemberAssignNotification.objects.all().order_by(id)[:100];
 for notification in pendingList.iterator():
     print(notification.members)
 
 print("INside Send Group Assign notifications",flush=True);
-sleep(300);#sleep for 5 mintes'''
-print (os.getcwd())
+sleep(300);#sleep for 5 mintes
