646ae1af37d21943d89be395ae4cc3168c29c066
vineeth0791 <vineethkumar0791@gmail.com>
2019-03-02 16:35:11 +0530

    PA always on task

--- signagecms/SendGroupAssignNotificationsBG.py
+++
@@ -1,7 +0,0 @@
-from django.db import connection
-
-query = "SELECT * FROM group_groupmemberassignnotification LIMIT 100";
-with connection.cursor() as cursor:
-    cursor.execute(query);
-    values = cursor.fetchall();
-print(query);
