893a6281bfef366af2cf7c86d6651d3bcd4ea0ca
vineeth0791 <vineethkumar0791@gmail.com>
2019-03-02 19:08:30 +0530

    Send Group assign notifications background

--- group/management/commands/SendGroupAssignNotificationsBG.py
+++ group/management/commands/SendGroupAssignNotificationsBG.py
@@ -1,9 +1,32 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from group.models import GroupMemberAssignNotification
+from django.core import mail
+from django.core.mail import EmailMessage
+import json
 
 class Command(BaseCommand):
     help = 'Sends Group assign notifications'
 
     def handle(self, *args, **kwargs):
-        time = timezone.now().strftime('%X')
-        self.stdout.write("It's now %s" % time)
+        #get notifications 
+        notifications = GroupMemberAssignNotification.objects.all().order_by('id')[:100];
+        if notifications.exists():
+            print("total notifications\{} ".format(notifications.count()),flush=True)
+            
+            from_email = "contact@adskite.com" 
+            try:
+                with mail.get_connection() as connection:
+                    for notification in notifications.iterator():
+                        msg = EmailMessage("Green Content Group Assign Notification", notification.message, to=json.loads(notification.member), from_email=from_email,
+                        connection=connection)
+                        msg.content_subtype = 'html'
+                        response = msg.send();
+                        print(notification.member,flush=True);
+                        print(response,flush=True);
+                        notification.delete();
+            except Exception as e:
+                print("Error in sending notifications to assigned"+str(e),flush=True);
+            
+        else:
+            print("No notifications found",flush=True);
