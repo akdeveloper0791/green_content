2fa3f8772e96e9abe02d9a8c316a64001dd9942f
vineeth0791 <vineethkumar0791@gmail.com>
2019-03-02 12:25:56 +0530

    long running tasks

--- group/SendGroupAssignNotificationsBG.py
+++ group/SendGroupAssignNotificationsBG.py
@@ -1,10 +1,20 @@
+
 from time import sleep
-from models import GroupMemberAssignNotification
+from django.core.mail import EmailMessage
+from django.db import connection
+
+
 
 #list latest 100 pending email requests
-pendingList = GroupMemberAssignNotification.objects.all().order_by(id)[:100];
-for notification in pendingList.iterator():
+query = "SELECT * FROM group_groupmemberassignnotification LIMIT 100 order by id ASC";
+with connection.cursor() as cursor:
+    cursor.execute(query);
+    values = cursor.fetchAll();
+print(query);
+
+'''pendingList = GroupMemberAssignNotification.objects.all().order_by(id)[:100];
+for notification `in pendingList.iterator():
     print(notification.members)
 
 print("INside Send Group Assign notifications",flush=True);
-sleep(300);#sleep for 5 mintes
+sleep(300);#sleep for 5 mintes'''
