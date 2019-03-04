77a52dcca38ff8550cbcd52ae424b77c743e02f8
vineeth0791 <vineethkumar0791@gmail.com>
2019-03-04 15:15:54 +0530

    multiple notifications beta

---
+++ group/management/commands/SendGroupAssignNotificationsBGBeta.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+import threading
+class Command(BaseCommand):
+    help = 'Sends Group assign notifications'
+
+    def handle(self, *args, **kwargs):
+    	thread1 = Thread1();
+    	thread1.start();
+    	#thread1.join();
+
+    	thread2 = Thread2();
+    	thread2.start();
+    	#thread2.join();
+
+
+
+class Thread1(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		for i in range(1000):
+			print("Thread1 - "+str(i));
+			++i;
+
+
+class Thread2(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		for i in range(1000):
+			print("Thread2 - "+str(i));
+			++i;
