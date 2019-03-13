6f774c632f09140d608bafcaffd2df4592a65fc0
vineeth0791 <vineethkumar0791@gmail.com>
2019-03-13 10:57:43 +0530

    login changes

--- cmsapp/urls.py
+++ cmsapp/urls.py
@@ -7,10 +7,10 @@ from campaign import views as cView
 urlpatterns = [
 
         url(r'^$',cView.listCampaignsWeb,name='listCampaignsWeb'),
-        url(r'^mycontent/$',views.home,name='home'),
+        url(r'^mycontent/$',cView.listCampaignsWeb,name='listCampaignsWeb'),
         url(r'^web_search/$',views.web_search,name='web_search'),
 
-        url(r'^search/$',views.home,name='search'),
+        url(r'^search/$',cView.listCampaignsWeb,name='listCampaignsWeb'),
 
 
 
