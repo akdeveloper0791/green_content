96a17c070a1c022824ed3b3aa4be755cd75ce817
vineeth0791 <vineethkumar0791@gmail.com>
2019-03-28 15:24:05 +0530

    License Registration

--- license/admin.py
+++ license/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import License_Device
 
 # Register your models here.
+class License_DeviceAdmin(admin.ModelAdmin):
+    list_display = ('mac', 'status', 'expiry_date', 'registered_date');
+    search_fields = ('mac',)
+    #search_fields = ("mac");
+    #list_per_page = 25;
+
+admin.site.register(License_Device,License_DeviceAdmin);
