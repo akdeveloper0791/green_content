0d0f67b05e3adac266d6620ed156ebe006a04287
vineeth0791 <vineethkumar0791@gmail.com>
2019-05-23 08:24:52 +0530

    schedule campaign

--- signagecms/settings.py
+++ signagecms/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 #ADMIN_MEDIA_PREFIX = '/admin/media/'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
