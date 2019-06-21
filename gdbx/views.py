cbca6d202c622b4ae5cb3998c249bdf3a75d046d
vineeth0791 <vineethkumar0791@gmail.com>
2019-06-21 11:38:58 +0530

    signageserv host

--- gdbx/views.py
+++ gdbx/views.py
@@ -9,6 +9,6 @@ def getGDbxxx(request):
         if((server=="www.greencontent.in" and host=="www.greencontent.in") or server=="DESKTOP-HARKF96" or (server=="www.signageserv.ai" and host=="www.signageserv.ai")):
             return JsonResponse({'xxdd':DROP_BOX_ACCESS_TOKEN});
         else:
-            return JsonResponse({'host':server});
+            return JsonResponse({'host':server,'host':host});
     else:
         return JsonResponse({});
