707db9961445706619dcb0dc424ddf443704dc18
vineeth0791 <vineethkumar0791@gmail.com>
2019-07-03 11:36:40 +0530

    player admin panel changes

--- player/admin.py
+++ player/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Player, Age_Geder_Metrics, Last_Seen_Metrics, Campaign_Reports
 
+class PlayerAdmin(admin.ModelAdmin):
+	list_display = ('mac', 'name');
+	search_fields = ('mac','name',);
+
 # Register your models here.
-admin.site.register(Player)
+admin.site.register(Player,PlayerAdmin)
 admin.site.register(Age_Geder_Metrics)
 admin.site.register(Last_Seen_Metrics)
 admin.site.register(Campaign_Reports)
+
