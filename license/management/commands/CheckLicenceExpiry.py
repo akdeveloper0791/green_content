f21f533001ed7411a8d10085f91f8f8c5de1cb7b
vineeth0791 <vineethkumar0791@gmail.com>
2019-08-23 14:59:26 +0530

    licence expiry check cron service

--- license/management/commands/CheckLicenceExpiry.py
+++ license/management/commands/CheckLicenceExpiry.py
@@ -10,14 +10,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         today = datetime.date.today();
-        oneyearDate = datetime.datetime.now()+datetime.timedelta(days=365);
-        License_Device.objects.all().update(expiry_date=oneyearDate);
-        print(oneyearDate.strftime("%Y-%m-%d %H:%M:%S"))
+        
         todayDateString = "{} {}".format(today.strftime("%Y-%m-%d"),
             "23:59:59")
         expiredDate = datetime.datetime.strptime(todayDateString,"%Y-%m-%d %H:%M:%S")
         expiredDate = expiredDate.astimezone(pytz.UTC);
         
         #get all expired devices
-        #expiredDevices = License_Device.objects.filter(Q(expiry_date__lte=expiredDate),Q(status=1) | Q(status=0)).update(status=-1);
-        #print(todayDateString+" updated :"+str(expiredDevices));
+        expiredDevices = License_Device.objects.filter(Q(expiry_date__lte=expiredDate),Q(status=1) | Q(status=0)).update(status=-1);
+        print(todayDateString+" updated :"+str(expiredDevices));
