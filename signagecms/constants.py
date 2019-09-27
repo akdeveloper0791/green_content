3627b22b645c312dc3ea6696de0c7a26171d80a4
vineeth0791 <vineethkumar0791@gmail.com>
2019-09-27 20:27:23 +0530

    access token

--- signagecms/constants.py
+++ signagecms/constants.py
@@ -1,4 +1,4 @@
-DROP_BOX_ACCESS_TOKEN = "mkW1hX251tAAAAAAAAAKvmx-522FKRlU4fNkICCfRu9sGRAVVz3kylZZBtq9jQX8"
+DROP_BOX_ACCESS_TOKEN = "mkW1hX251tAAAAAAAAAKwHfZYe6SXtMZDOjnrmr4YDQdU4HHdcddSNo_Hjj34Azc"
 API_HOST = "https://www.greencontent.in"
 ADSKITE_PLAYER_REGISTER_PWD="!AdsKite2019$"
 SINGLE_100_100 = "{\"regions\":[{\"width\":100,\"height\":100,\"top_margin\":0,\"left_margin\":0,\"right_margin\":0,\"bottom_margin\":0}]}"
