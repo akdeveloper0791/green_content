4890cd2e103d50292297ae7977ccf68a9c416873
anilyerrasani <anilyerrasani@gmail.com>
2019-11-12 12:14:23 +0530

    Home page UI changes

--- gdbx/views.py
+++ gdbx/views.py
@@ -10,7 +10,7 @@ def getGDbxxx(request):
     if(request.method == "POST" and request.user.is_authenticated):       
         host = request.META['HTTP_HOST'];
         server = request.META['SERVER_NAME'];
-        if((server=="www.signageserv.ai" and host=="www.signageserv.ai") or host=="192.168.0.143:8080" or host=="www.sunriosignage.com" or (server=="www.ihealthtv.com" and host=="www.ihealthtv.com")):
+        if((server=="www.signageserv.ai" and host=="www.signageserv.ai") or host=="192.168.0.143:8080" or host=="www.sunriosignage.com" or (server=="www.ihealthtv.com" and host=="www.ihealthtv.com") or (server=="www.nextgenlearning.in" and host=="www.nextgenlearning.in")):
             return JsonResponse({'xxdd':DROP_BOX_ACCESS_TOKEN});
         else:
             return JsonResponse({});
